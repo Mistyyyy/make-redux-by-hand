4d5c76e3568f473027341116b5f3cfa4ce98cc98
dingchunlei <dingchunlei@shein.com>
2018-11-25 18:58:42 +0800

    重置combineReducers的参数

--- src/store.js
+++ src/store.js
@@ -23,7 +23,7 @@ function createStore (reducer, initState = {}) {
 // }
 
 const combineReducers = reducers => {
-  return (state = {}, action) => {
+  return (state, action) => {
     return Object.keys(reducers).reduce((nextState, key) => {
       nextState[key] = reducers[key](state[key], action)
       return nextState
